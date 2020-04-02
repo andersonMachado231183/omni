5bb4911fffd803289de414622d29c8aa0ab36d48
andersonMachado231183 <pitt231183@gmail.com>
2020-04-01 22:17:39 -0300

    logon

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -2,10 +2,21 @@ import React from 'react';
 
 import './styles.css';
 
+import imgHeroes from '../../assets/heroes.png';
+import imgLogo from '../../assets/logo.svg';
+
 export default function Logon(){
     return(
-        <h1>
-            Hello World
-        </h1>
+    <div className="logon-container">
+        <section className="form">
+            <img src={ imgLogo } alt="logo"/>
+
+            <form><h1>Faça seu logon</h1></form>
+            <input placeholder="Sua ID"/>
+            <button type="submit">Entrar</button>
+        </section>
+
+        <img src={imgHeroes } alt="heroes"/>
+    </div>
     );
 }
