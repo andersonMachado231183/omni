9fe4c6cfe1433229da9a441845c26be6ee5653db
andersonMachado231183 <pitt231183@gmail.com>
2020-04-02 22:01:20 -0300

    input

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import './styles.css';
+import { FiLogIn } from 'react-icons/fi';
 
 import imgHeroes from '../../assets/heroes.png';
 import imgLogo from '../../assets/logo.svg';
@@ -14,9 +15,13 @@ export default function Logon(){
             <form><h1>Faça seu logon</h1></form>
             <input placeholder="Sua ID"/>
             <button type="submit">Entrar</button>
+            <a href="./register">
+                <FiLogIn size={16} color="#e02041" />
+                Nao tenho cadastro
+            </a>
         </section>
 
-        <img src={imgHeroes } alt="heroes"/>
+        <img src= { imgHeroes } alt="heroes"/>
     </div>
     );
 }
